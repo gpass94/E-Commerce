3c66ec471c5560060786a056e6d9035241607658
Alan Richards <alanrichards86@gmail.compwdlsexitstopopengitopengit config --global user.name Alan>
2017-07-28 16:12:28 -0400

    Added - Components folder, styles folder, app.js. baselayout.js, and also added browser router to the index.js. Page was working when pushed.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import BaseLayout from './components/BaseLayout';
+
+ReactDOM.render(
+
+  <BrowserRouter>
+    <BaseLayout>
+
+      <Switch>
+        <Route exact path='/' component={BaseLayout} />
+      </Switch>
+
+    </BaseLayout>
+  </BrowserRouter>
+
+
+,document.getElementById('root'));
 registerServiceWorker();
