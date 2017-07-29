76ff31e4277c93d7ba4b6862ff8ceb49f251c0ed
Corianioh <corianoharris@gmail.com>
2017-07-29 15:45:30 -0400

    added checkout component and css

---
+++ src/components/Checkout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/Checkout.css';
+
+export default class Checkout extends Component {
+    constructor(){
+        super();
+    }
+
+    render( {
+
+        return ()
+
+            )
+        }
+
+    }    
