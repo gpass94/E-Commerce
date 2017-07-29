170ac63937aa1cf46f26db649a704e983349fac5
Curtis Frazier <fraziercurtis2003@yahoo.com>
2017-07-29 15:38:44 -0400

    initial Entertainment component

---
+++ src/components/Entertainment.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {NavLink} from 'react-router-dom';
+import '../styles/Entertainment.css'
+
+
+export default class Entertainment extends Component {
+  render() {
+    return(
+      <div className='entertainmentDiv'>
+        <p> Entertainment Page Working</p>
+      </div>
+    )
+  }
+}
