10dcb1346c1deb8120837693324aa3053f293ea8
Grant Paschal <gpass94@gmail.com>
2017-07-30 18:54:11 -0400

    Clothing Additions

--- src/components/Clothing.js
+++ src/components/Clothing.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
+import '../styles/Clothing.css';
 
 export default class Clothing extends Component {
   render() {
     return(
       <div className='clothing'>
-        <p>Clothing is working</p>
-        <h2>T-Shirts</h2>
-        <h2>Pants</h2>
-        <h2>Suits</h2>
+        <div className='categories-clothing'>
+          <img src= '../imgs/clothing/TShirt.jpg' alt='Tshirt'/>
+          <h2>T-Shirts</h2>
+          <p>Here are our designer line speciality T-Shirts</p>
+        </div>
+        <div className='categories-clothing'>
+          <img src= '../imgs/clothing/Pants.jpeg' alt='pants'/>
+          <h2>Pants</h2>
+          <p>Here are our designer line speciality Pants</p>
+        </div>
+        <div className='categories-clothing'>
+          <img src= '../imgs/clothing/MensSuit.jpeg' alt='suits'/>
+          <h2>Suits</h2>
+          <p>Here are our designer line speciality Suits</p>
+        </div>
       </div>
     )
   }
