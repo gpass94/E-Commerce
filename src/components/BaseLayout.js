edd391a1479331cb50f6672ff16510abb545fb0e
Curtis Frazier <fraziercurtis2003@yahoo.com>
2017-07-29 22:08:28 -0400

    added product sections

--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -25,7 +25,7 @@ export default class BaseLayout extends Component {
                     <NavLink activeClassName='selected' className="nav-link" to="/appliances">Appliances</NavLink>
                   </li>
 
-<<<<<<< HEAD
+
                   <li className="nav-item">
                     <NavLink activeClassName='selected' className="nav-link" to="/clothing">Clothing</NavLink>
                   </li>
@@ -34,7 +34,7 @@ export default class BaseLayout extends Component {
                     <NavLink activeClassName='selected' className="nav-link" to="/entertainment">Entertainment</NavLink>
                   </li>
 
-=======
+
                   <li className ="nav-item">
                     <NavLink activeClassName='selected' className="nav-link" to="/clothing">Clothing</NavLink>
                   </li>
@@ -42,7 +42,7 @@ export default class BaseLayout extends Component {
                   <li className="nav-item">
                     <NavLink activeClassName='selected' className="nav-link" to="/checkout">Checkout</NavLink>
                   </li>
->>>>>>> 6709904e0406be99fe921f8ad1064325506732fd
+
                 </ul>
             </div>
           </div>
--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ import Books from './components/Books';
 import ContactUs from './components/ContactUs';
 import Appliances from './components/Appliances';
 import Clothing from './components/Clothing';
-<<<<<<< HEAD
+
 import Entertainment from './components/Entertainment';
-=======
+
 import Checkout from './components/Checkout';
->>>>>>> 6709904e0406be99fe921f8ad1064325506732fd
+
 
 
 
@@ -31,11 +31,11 @@ ReactDOM.render(
         <Route path='/contactUs' component={ContactUs} />
         <Route path='/appliances' component={Appliances} />
         <Route path='/clothing' component={Clothing} />
-<<<<<<< HEAD
+
         <Route path='/entertainment' component={Entertainment} />
-=======
+
         <Route path='/checkout' component={Checkout} />
->>>>>>> 6709904e0406be99fe921f8ad1064325506732fd
+
 
       </Switch>
 
