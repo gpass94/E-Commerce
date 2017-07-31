c7c5b6a1e3af3890fe1bd16cc604601cddefb6e7
Grant Paschal <gpass94@gmail.com>
2017-07-31 09:20:22 -0400

    Corrected Images

--- src/components/Clothing.js
+++ src/components/Clothing.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react'
 import '../styles/Clothing.css';
+import TShirt from '../imgs/clothing/MensSuit.jpeg';
+import Pants from '../imgs/clothing/Pants.jpeg';
+import Suits from '../imgs/clothing/MensSuit.jpeg';
+
+
 
 export default class Clothing extends Component {
   render() {
     return(
       <div className='clothing'>
         <div className='categories-clothing'>
-          <img src= '../imgs/clothing/TShirt.jpg' alt='Tshirt'/>
+          <img src={TShirt} alt='Tshirt'/>
           <h2>T-Shirts</h2>
           <p>Here are our designer line speciality T-Shirts</p>
         </div>
         <div className='categories-clothing'>
-          <img src= '../imgs/clothing/Pants.jpeg' alt='pants'/>
+          <img src= {Pants} alt='pants'/>
           <h2>Pants</h2>
           <p>Here are our designer line speciality Pants</p>
         </div>
         <div className='categories-clothing'>
-          <img src= '../imgs/clothing/MensSuit.jpeg' alt='suits'/>
+          <img src={Suits} alt='suits'/>
           <h2>Suits</h2>
           <p>Here are our designer line speciality Suits</p>
         </div>
