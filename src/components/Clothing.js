cb9340a49fd0930e022ec3dab9486cb7f1cbbb20
Grant Paschal <gpass94@gmail.com>
2017-07-31 09:41:55 -0400

    Final Images in Clothing

--- src/components/Clothing.js
+++ src/components/Clothing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import '../styles/Clothing.css';
+import '../styles/BaseLayout.css';
 import TShirt from '../imgs/clothing/TShirt.jpg';
 import Pants from '../imgs/clothing/Pants.jpeg';
 import Suits from '../imgs/clothing/MensSuit.jpeg';
@@ -10,17 +11,17 @@ export default class Clothing extends Component {
   render() {
     return(
       <div className='clothing'>
-        <div className='categories-clothing'>
+        <div className='categories-tshirt'>
           <img src={TShirt} alt='Tshirt'/>
           <h2>T-Shirts</h2>
           <p>Here are our designer line speciality T-Shirts</p>
         </div>
-        <div className='categories-clothing'>
+        <div className='categories-pants'>
           <img src= {Pants} alt='pants'/>
           <h2>Pants</h2>
           <p>Here are our designer line speciality Pants</p>
         </div>
-        <div className='categories-clothing'>
+        <div className='categories-suits'>
           <img src={Suits} alt='suits'/>
           <h2>Suits</h2>
           <p>Here are our designer line speciality Suits</p>
