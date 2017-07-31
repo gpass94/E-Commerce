d5cc3cd468bdc79dc459b128c592aec733d5e33b
Grant Paschal <gpass94@gmail.com>
2017-07-31 13:38:18 -0400

    Added Prices

--- src/components/Clothing.js
+++ src/components/Clothing.js
@@ -14,16 +14,19 @@ export default class Clothing extends Component {
         <div className='categories-tshirt'>
           <img src={TShirt} alt='Tshirt'/>
           <h2>T-Shirts</h2>
+          <h6>15.99</h6>
           <p>Here are our designer line speciality T-Shirts</p>
         </div>
         <div className='categories-pants'>
           <img src= {Pants} alt='pants'/>
           <h2>Pants</h2>
+          <h6>25.99</h6>
           <p>Here are our designer line speciality Pants</p>
         </div>
         <div className='categories-suits'>
           <img src={Suits} alt='suits'/>
           <h2>Suits</h2>
+          <h6>149.99</h6>
           <p>Here are our designer line speciality Suits</p>
         </div>
       </div>
